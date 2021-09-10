d369d88942c6f51b4f5aadc29dd24e52b25998a3
Matthew <matthew.j.klimek@gmail.com>
2021-09-10 13:34:47 -0400

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+ purge: [],
+ darkMode: false, // or 'media' or 'class'
+ theme: {
+  colors: {
+   light: "#eeeeee",
+   medium: "#344246",
+   medium50: "#34424679",
+   dark: "#1E292A",
+   primary: "#FFB900",
+   secondary: "#00C2EF",
+  },
+ },
+
+ variants: {
+  extend: {},
+ },
+ plugins: [],
+};
